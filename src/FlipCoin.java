3a901e7b9a6cb26d2523537d49cad383daf5865f
Theroiz <164636377+Theroiz@users.noreply.github.com>
2024-06-12 21:26:12 -0300

    Add files via upload

--- src/FlipCoin.java
+++ src/FlipCoin.java
@@ -8,6 +8,12 @@ public class FlipCoin {
         Random rand = new Random();
         //double chance = rand.nextDouble(0.0,1.0);
         int chance = rand.nextInt(50) + 2;
+        //                         ↟     ↟
+        //                         |     segundo valor "inicial / apartir"
+        //                         valor a gerar "limite" para o segundo valor
+        // ** a partir de 2 ira gerar ate 50 numeros( 3,10,2,48 )
+        
+        
         System.out.println(chance);
         /*if(chance < 0.50)
         {
