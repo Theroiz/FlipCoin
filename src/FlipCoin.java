f3b838bc3c24737f948b82edf6171b2c072fb65e
Theroiz <164636377+Theroiz@users.noreply.github.com>
2024-06-12 21:01:42 -0300

    Add files via upload

---
+++ src/FlipCoin.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class FlipCoin {
+
+    public static void main(String[] args) {
+
+        // 50% chance heads, 50% chance tails
+        Random rand = new Random();
+        double chance = rand.nextDouble(0.0,1.0);
+        System.out.println(chance);
+        if(chance < 0.50)
+        {
+            System.out.println("Cara!");
+        }else
+        {
+            System.out.println("Coroa!");
+        }
+        }
+    }
+
